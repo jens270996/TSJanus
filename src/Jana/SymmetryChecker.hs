8c6bd3ee560d0dd8acbb0a2fd3bd2c75989e5875
Jens Kristensen <jens-nk@hotmail.com>
2025-03-03 13:58:17 +0100

    cleanup

--- src/Jana/SymmetryChecker.hs
+++ src/Jana/SymmetryChecker.hs
@@ -14,11 +14,11 @@ checkSymmetryId (Program _ procs) funId =
 checkSymmetryStmts ::  [Proc] -> [Stmt] -> Bool
 checkSymmetryStmts procs stmts =
     case stmts of
-        [] -> True 
-        [s] -> if checkSymmetryStmt procs s then True else error "fail here"
+        [] -> True
+        [s] -> checkSymmetryStmt procs s
         stmts -> case uncons stmts
-                 of Just (s, [s2]) -> if s `equiv` (invertStmt Locally s2) then True else error "two statement fail"
-                    Just (s,stmts') -> if checkSymmetryStmts procs (init stmts') && (invertStmt Locally (last stmts')) `equiv` s then True else error (show (invertStmt Locally (last stmts')))
+                 of Just (s, [s2]) -> s `equiv` invertStmt Locally s2
+                    Just (s,stmts') -> checkSymmetryStmts procs (init stmts') && invertStmt Locally (last stmts') `equiv` s
                     Nothing -> error "should never reach here as we already checked that (length stmts) >= 2"
 
 checkSymmetryStmt :: [Proc] -> Stmt-> Bool
@@ -31,4 +31,4 @@ checkSymmetryStmt procs s =
         (Uncall funId _ _) -> case filter (\procedure -> procname procedure == funId ) procs of
                                 [procedure] -> checkSymmetryStmts procs (body procedure)
                                 _ -> error "invalid procedure id"
-        s -> if s == invertStmt Locally s then True else error (show (formatStmt s))
+        s -> s `equiv` invertStmt Locally s
